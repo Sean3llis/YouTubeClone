166fe39b7986043dfbab3b583474d1abd08b8d37
Sean Ellis <seane235@gmail.com>
2016-01-30 16:08:55 -0500

    es6 refactor

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends Component {
+  render() {
+    return (
+      <input onChange={
+        e => console.log(e.target.value)
+      } />
+    );
+  }
+
+  onInputChange(e) {
+    console.log(e.target.value);
+  }
 }
 
 export default SearchBar;
