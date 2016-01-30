a63b8d21c0669e5e36e6312a9d71b96dbeb9e687
Sean Ellis <seane235@gmail.com>
2016-01-30 15:49:14 -0500

    add API key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import API_KEY from './private/api_key'
 
 const App = () => {
   return <div>Hello World!</div>
 }
+console.log(API_KEY);
 
 ReactDOM.render(<App />, document.getElementById('app'));
