bdace294f7efb22c4281ce1eece15ca0257f6b5f
Sean Ellis <seane235@gmail.com>
2016-01-31 11:27:20 -0500

    live updating search, debounced

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const VideoDetail  = ({video}) => {
-  if (!video) return <div>loading...</div>;
-  const videoId = video.id.videoID;
-  const url = `https://www.youtube.com/embed/${videoId}`;
   console.log(video);
+  if (!video) return <div>loading...</div>;
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={url}></iframe>
+        <iframe className="embed-responsive-item" src={url} frameBorder="0" allowFullScreen></iframe>
       </div>
       <div className="detail">
         <div>{video.snippet.title}</div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-search-api';
 import API_KEY from './private/api_key';
+import _ from 'lodash';
 
 // Components:
 import SearchBar from './components/search_bar';
@@ -17,8 +18,12 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     }
+    this.videoSearch('texas longhorns')
 
-    YTSearch({key: API_KEY, term: 'texas longhorns'}, videos => {
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -28,9 +33,10 @@ class App extends Component {
 
 
   render() {
+    const videoSearch = _.debounce((term) => this.videoSearch(term), 300);
     return (
       <div className="container">
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <div className="row">
           <VideoDetail video={this.state.selectedVideo}/>
           <VideoList
